66b9b397a2f31bd5a5ca8028c32743fdba407923
Mutiara Noor Fauzia <143862669+moetzi@users.noreply.github.com>
2023-12-08 21:45:47 +0700

    Adding group name

--- sudoku/src/sudoku/Solver.java
+++ sudoku/src/sudoku/Solver.java
@@ -1,3 +1,12 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2023/2024
+ * Group Capstone Project
+ * Group #4
+ * 1 - 5026221045 - Mutiara Noor Fauzia
+ * 2 - 5026221096 - Viera Tito Virgiawan
+ * 3 - 5026221193 - Maureen Ghassani Fadhliphya
+ */
 package sudoku;
 public class Solver {
 
