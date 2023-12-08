fddb4ae242bf36636b42ab3bbc354c2ad683a6f6
fadhliphya <152939794+fadhliphya@users.noreply.github.com>
2023-12-08 21:17:19 +0700

    Adding group names

--- tictactoe/src/Cell.java
+++ tictactoe/src/Cell.java
@@ -1,3 +1,13 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2023/2024
+ * Group Capstone Project
+ * Group #4
+ * 1 - 5026221045 - Mutiara Noor Fauzia
+ * 2 - 5026221096 - Viera Tito Virgiawan
+ * 3 - 5026221193 - Maureen Ghassani Fadhliphya
+ */
+
 import java.awt.*;
 public class Cell {
     // Define named constants for drawing
