8e11fcf5672cd2f930312b5011b2ab1d5191080a
Viera Tito <129572101+Ergiaa@users.noreply.github.com>
2023-12-08 22:07:00 +0700

    Solve and Hint Function Added

--- sudoku/src/sudoku/SudokuMain.java
+++ sudoku/src/sudoku/SudokuMain.java
@@ -7,15 +7,18 @@ import java.util.Random;
 
 public class SudokuMain extends JFrame {
     private static final long serialVersionUID = 1L;
-    public static int input;
+    public static int input = 1;
 
     private int secondsPassed;
 
     private GameBoardPanel board;
     private JButton btnNewGame;
     private JButton btnHint;
+    private JButton btnSolve;
+    private JPanel btnPanel;
     private Timer timer;
     private JLabel timerLabel;
+    private int hintCount;
 
     public SudokuMain() {
         Container cp = getContentPane();
@@ -37,42 +40,69 @@ public class SudokuMain extends JFrame {
 
         // Create instances of GameBoardPanel and InputBar
         board = new GameBoardPanel(timer);
+        btnPanel = new JPanel(new GridLayout());
 
         // Create a button to start a new game
         btnNewGame = new JButton("New Game");
         btnHint = new JButton("Hint");
+        btnSolve = new JButton("Solve");
         btnNewGame.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 secondsPassed = 0;
+                hintCount = 0;
                 updateTimerLabel();
                 board.newGame();
             }
         });
-//        btnHint.addActionListener(e -> {
-//            Random r = new Random();
-//            int row = 0, col = 0;
-//            for(int i = 0; i < 9; i++){
-//                for(int j = 0; j < 9; j++){
-//                    if(!board.getPuzzle().isGiven[i][j]){
-//                        row = i;
-//                        col = j;
-//                        break;
-//                    }
-//                }
-//                if(!board.getPuzzle().isGiven[row][col]){
-//                    break;
-//                }
-//            }
-//            board.getCells()[row][col].setText("" + board.getPuzzle().numbers[row][col]);
-//            board.getCells()[row][col].paint();
-//            if (board.isSolved()) {
-//                timer.stop();
-//                JOptionPane.showMessageDialog(null, "Congratulations! You've solved the puzzle!");
-//            }
-//        });
-        cp.add(btnNewGame, BorderLayout.SOUTH);
-//        cp.add(btnHint, BorderLayout.LINE_END);
+        btnHint.addActionListener(e -> {
+            if(hintCount < 3) {
+                Random r = new Random();
+                int row = 0, col = 0;
+                boolean found = false;
+                for (int i = 0; i < 9; i++) {
+                    for (int j = 0; j < 9; j++) {
+                        if (board.getCells()[i][j].status == CellStatus.TO_GUESS || board.getCells()[i][j].status == CellStatus.WRONG_GUESS) {
+                            row = i;
+                            col = j;
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found) {
+                        found = false;
+                        break;
+                    }
+                }
+                board.getCells()[row][col].setText("" + board.getPuzzle().numbers[row][col]);
+                board.getCells()[row][col].status = CellStatus.CORRECT_GUESS;
+                board.getCells()[row][col].paint();
+                hintCount++;
+                if (board.isSolved()) {
+                    timer.stop();
+                    JOptionPane.showMessageDialog(null, "Congratulations! You've solved the puzzle!");
+                }
+            }
+        });
+        btnSolve.addActionListener(e -> {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if(board.getCells()[i][j].status == CellStatus.TO_GUESS || board.getCells()[i][j].status == CellStatus.WRONG_GUESS) {
+                        board.getCells()[i][j].setText("" + board.getPuzzle().numbers[i][j]);
+                        board.getCells()[i][j].status = CellStatus.CORRECT_GUESS;
+                        board.getCells()[i][j].paint();
+                        if (board.isSolved()) {
+                            timer.stop();
+                            JOptionPane.showMessageDialog(null, "Here is the solved puzzle!");
+                        }
+                    }
+                }
+            }
+        });
+        btnPanel.add(btnNewGame);
+        btnPanel.add(btnHint);
+        btnPanel.add(btnSolve);
+        cp.add(btnPanel, BorderLayout.SOUTH);
 
         // Add the GameBoardPanel and InputBar to the center of the layout
         cp.add(board, BorderLayout.CENTER);
