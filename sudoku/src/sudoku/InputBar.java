f7ab22d080e147353a38117881e4119809f3c47f
moetzi <mutiaranoorfauzia@gmail.com>
2023-12-19 02:23:57 +0700

    About btn arrangement for both games + add restart btn in ttt

--- sudoku/src/sudoku/InputBar.java
+++ sudoku/src/sudoku/InputBar.java
@@ -9,10 +9,7 @@
  */
 package sudoku;
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class InputBar extends JPanel {
     JButton[] buttons;
--- tictactoe/src/About.java
+++ tictactoe/src/About.java
@@ -7,18 +7,17 @@
  * 2 - 5026221096 - Viera Tito Virgiawan
  * 3 - 5026221193 - Maureen Ghassani Fadhliphya
  */
-package sudoku;
 
 import javax.swing.*;
 
-public class AboutFrame extends JFrame {
-    public AboutFrame() {
+public class About extends JFrame {
+    public About() {
             setSize(400, 600);
             setLocationRelativeTo(null);
             
             ImageIcon image = new ImageIcon("sudoku/src/sudoku/img/dev.png");
 
-            JLabel label = new JLabel("<html><br><center>Sudoku Game</center><center>Version 1.0</center><br><center>Developed by Group 4:</center><br>"
+            JLabel label = new JLabel("<html><br><center>Tic Tac Toe</center><center>Version 1.0</center><br><center>Developed by Group 4:</center><br>"
             + "<center>5026221096 - Viera Tito Virgiawan</center>"
             + "<center>5026221045 - Mutiara Noor Fauzia</center>"
             + "<center>5026221193 - Maureen Ghassani Fadhliphya</center><br>"
